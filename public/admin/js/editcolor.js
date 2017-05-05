b6fc9ff77350fbab3f0605dd8d87f2160c34b746
tranvanmy <anhmynd19101995@gmail.com>
2017-05-05 14:57:58 +0700

    init project
    static page
    
    delete css
    
    static page 2
    
    static page3

---
+++ public/admin/js/editcolor.js
@@ -0,0 +1,15 @@
+$('.listcolor').click(function(event) {
+	var id = $(this).attr('data-id');
+	
+	 $.ajax({
+                    url : "xem-color?id="+id,
+                    type : "get",
+                    dataType:"text",
+                  
+                    success : function (result){
+                        $('.modal-body1').html(result);
+                    }
+                });
+	 event.preventDefault();
+  $('#modal-id1').modal('show');
+});
